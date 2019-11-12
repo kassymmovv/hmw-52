20bf52c5e8b0ad6467803ee249ad3720a45b485a
Ayan Kasymov <maxxlvl1@mail.ru>
2019-11-12 18:37:56 +0700

    added function generateNum

--- src/App.js
+++ src/App.js
@@ -1,1 +1,43 @@
+import React from 'react';
+import Circle from "./Components/Circle/Circle";
 
+class App extends Component {
+    state = {
+        numbers: []
+    };
+    generateNum = () => {
+
+        let array = [];
+        for (let i = 0; i < 5;) {
+            const number = Math.floor(Math.random() * 36) + 5;
+            if (array.includes(number)) {
+                continue;
+            } else {
+                array.push(number);
+                i++
+            }
+
+        }
+        let stateArray = [...this.state.numbers];
+        stateArray = array;
+        this.setState({
+            numbers: stateArray
+        })
+
+    };
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.numbers.map((dcc,id)=>{
+                    return <Circle key={id} number={dcc} />
+                })}
+                <button onClick={this.generateNum}>
+                    generate
+                </button>
+            </div>
+        )
+    }
+}
+
+export default App;
